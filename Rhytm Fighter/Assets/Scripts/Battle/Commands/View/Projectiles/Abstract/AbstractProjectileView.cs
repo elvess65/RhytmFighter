c0f5014a4f9f75c044f806f615657560d8f1d0b3
elvess65 <37631917+elvess65@users.noreply.github.com>
2020-04-25 17:06:23 +0300

    ProjectileExplosion

--- Rhytm Fighter/Assets/Scripts/Battle/Commands/View/Projectiles/Abstract/AbstractProjectileView.cs
+++ Rhytm Fighter/Assets/Scripts/Battle/Commands/View/Projectiles/Abstract/AbstractProjectileView.cs
@@ -4,11 +4,21 @@ namespace RhytmFighter.Battle.Command.View
 {
     public abstract class AbstractProjectileView : AbstractCommandView
     {
+        public GameObject CollisionEffectPrefab;
+
         public virtual void Initialize(Vector3 targetPos, Vector3 senderPos, float existTime)
         {
             m_LerpData.To = targetPos;
 
             base.Initialize(senderPos, existTime);
+        }
+
+        protected override void FinalizeView()
+        {
+            base.FinalizeView();
+
+            GameObject collisionEffect = Instantiate(CollisionEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(collisionEffect.gameObject, 2);
         }
     }
 }
